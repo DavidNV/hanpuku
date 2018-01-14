a5dabbee014645dccad7103b3202fb9e2aae8f6f
Orferus <david.nvalderrama@gmail.com>
2018-01-14 18:58:11 -0500

    Config for deploy

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,8 +1,9 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
+  config.application_name = "Hanpuku"
   config.api_key = "<api_key>"
   config.secret = "<secret>"
   config.scope = "read_orders, read_products"
+  config.redirect_uri = "http://hanpuku.herokuapp.com/auth/shopify/callback"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
